1913506f3935514df7283e69ceee76df70e5df39
Daniel Pasut <daniel.j.pasut@gmail.com>
2024-09-11 13:39:35 -0400

    Attempt at fixing index.json url.

--- static/js/custom.js
+++ static/js/custom.js
@@ -42,7 +42,7 @@ u('#searchButton').handle('click', function (e) { //use handle to automatically 
 })
 
 function executeSearch(searchQuery) {
-  fetch(window.hugoBaseURL + "index.json").then(r => r.json()).then(function (data) {
+  fetch(window.hugoBaseURL + "/index.json").then(r => r.json()).then(function (data) {
     var pages = data;
     var fuse = new Fuse(pages, fuseOptions);
     var result = fuse.search(searchQuery);
